2ad1ad08ac5b4f18571d39be046f842aa6e3abe3
Fritz <blueloke@gmail.com>
2025-04-03 10:27:56 -0700

    analytics and insights added

--- ui/app/layout.tsx
+++ ui/app/layout.tsx
@@ -3,6 +3,8 @@
 import "@/styles.globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
